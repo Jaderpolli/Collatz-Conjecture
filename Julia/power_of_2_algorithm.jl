949ffbed8ec93532b8567e9409ef959a6aefda9b
Jaderson <jaderpolli99@gmail.com>
2022-03-30 16:08:49 -0300

    Created the algorithm to save base 10 from m-vectors

--- Julia/power_of_2_algorithm.jl
+++ Julia/power_of_2_algorithm.jl
@@ -10,26 +10,51 @@ Next step is make it write a file
 with the result in the RAW_DATA/
 directory =#
 
-function algoritmo(n)
-    M = n
-    ms = []
-    while M > 1
-        m = 0
-        p = M
-        while p % 2 == 0
-            p = p/2
-            m = m+1
+# ALGORITHMS
+
+module Algorithms
+    function algorithm(n::BigInt)
+        M = BigInt(n)
+        ms = BigInt[]
+        while M > 1
+            m = BigInt(0)
+            p = BigInt(M)
+            while p % 2 == 0
+                p = div(p,2)
+                m = BigInt(m+1)
+            end
+            ms = vcat(ms, m)
+            M = div(M,2^m)-1
         end
-        push!(ms, m)
-        M = M/2^m-1
+        return(ms)
     end
-    return(ms)
+
+    function rev_algorithm(ms)
+        n = BigInt(0)
+        for i in 1:length(ms)
+            n = BigInt(n)+BigInt(2)^(sum(ms[1:i]))
+        end
+        return n
+    end
 end
 
-function rev_algoritmo(ms)
-    n = BigInt(0)
-    for i in 1:length(ms)
-        n = BigInt(n)+BigInt(2)^(sum(ms[1:i]))
+# READ m-vector and return base 10 number
+
+module Saving
+
+    import Main.Algorithms
+    using CSV
+    using DataFrames
+    using DelimitedFiles
+
+    function rev_save()
+        m = CSV.read("RAW_DATA/INITIAL_CONDITIONS/n_0_2_power_of_2.csv", DataFrame)
+        m = Vector(m[:,1])
+        n = Algorithms.rev_algorithm(m)
+        writedlm("RAW_DATA/INITIAL_CONDITIONS/n_0_2_base10.csv", n)
     end
-    return n
+
+    rev_save()
 end
+
+import Main.Saving
