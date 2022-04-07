e1cde572fce6483c8eeb402701d44ad6e4629d19
jaderson <jaderpolli99@gmail.com>
2022-04-07 12:51:41 -0300

    Add the module to create the stationary orbit (update)

--- Julia/power_spectra_modules.jl
+++ Julia/power_spectra_modules.jl
@@ -4,43 +4,38 @@ using DelimitedFiles
 type = "Random"
 var = readdlm("RAW_DATA/INITIAL_CONDITIONS/variables_n_0_$(type).dat", header = false)
 var = Array(var)
-vecsize = var[1] #size of the m-vector
-maxrand = var[2] #when type = "Random" this is the maximum value possible
-blocksize = var[3] # when type = "Prime" this is the number of the first primes taken
+mVectorSize = var[1] #size of the m-vector
+MaxRand = var[2] #when type = "Random" this is the maximum value possible
+primeBlockSize = var[3] # when type = "Prime" this is the number of the first primes taken
 
 # This module contains the functions for generating the stationary orbit from collatz base 10
 module StationaryOrbits
     using DelimitedFiles
     function gamma_2(i::Int64; type::String)
-        if type == "Prime"
-            orb = readdlm("RAW_DATA/ORBITS/orb_n_0_$(i)_$(type)_vecsize_$(vecsize)_blocksize_$(blocksize)_base10.csv", BigInt, header = false)
-        elseif type == "Random"
-            orb = readdlm("RAW_DATA/ORBITS/orb_n_0_$(i)_$(type)_vecsize_$(vecsize)_maxrand_$(maxrand)_base10.csv", BigInt, header = false)
-        end
-        orb = log2.(orb)
-        dy = orb[end]-orb[1]
-        dx = length(orb)
-        γ₂ = dy/dx
-        return(orb,γ₂)
+        orbit = readdlm("RAW_DATA/ORBITS/orb_n_0_$(i)_$(type)_mVectorSize_$(mVectorSize)_MaxRand_$(MaxRand)_primeBlockSize_$(primeBlockSize)_base10.csv", BigInt, header = false)
+        orbit = log2.(orbit)
+        Δy = orbit[end]-orbit[1]
+        Δx = length(orb)
+        γ₂ = Δy/Δx
+        return(orbit,γ₂)
     end
 
-    function fit_gamma_2(orb, γ₂)
-        dx = length(orb)
-        fit = zeros(dx)
-        n = range(1, dx, length = dx)
-        for i in 1:dx
-            fit[i] = orb[1]+γ₂*i
+    function fit_gamma_2(orbit, γ₂)
+        Δx = length(orbit)
+        fit = zeros(Δx)
+        for i in 1:Δx
+            fitFunction[i] = orbit[1]+γ₂*i
         end
-        return(fit)
+        return(fitFunction)
     end
 
-    function stacionary(orb, fit)
-        dx = length(orb)
-        dif = zeros(dx)
-        for i in 1:dx
-            dif[i] = orb[i] - fit[i]
+    function stacionary(orbit, fitFunction)
+        Δx = length(orbit)
+        differenceOrbitFit = zeros(Δx)
+        for i in 1:Δx
+            differenceOrbitFit[i] = orbit[i] - fitFunction[i]
         end
-        return(dif)
+        return(differenceOrbitFit)
     end
 end
 
@@ -57,6 +52,6 @@ module PowerSpectra
         fit = fit_gamma_2(orb,γ₂)
         dif = stacionary(orb,fit)
         fft_dif = abs.(fft(dif))
-        ps_dif = (fft_dif).^2
+        ps_dif = fft_dif.^2
     end
 end
