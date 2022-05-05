afd8cafb881242a92f5f84e722296538aa20b151
jaderson <jaderpolli99@gmail.com>
2022-05-05 16:46:58 -0300

    Create code for gamma_2 histogram and update power spectra histogram

--- Julia/FIGURES_SCRIPTS/GAMMA_2_FIGURES_SCRIPTS/gamma_2_histograms.jl
+++ Julia/FIGURES_SCRIPTS/GAMMA_2_FIGURES_SCRIPTS/gamma_2_histograms.jl
@@ -4,7 +4,7 @@ using StatsBase
 using LaTeXStrings
 using Plots.PlotMeasures
 
-pasta = "FIGURES/POWER_SPECTRA_FIGURES"
+pasta = "FIGURES/GAMMA_2_FIGURES"
 mkpath(pasta)
 
 function histograms()
@@ -18,61 +18,61 @@ function histograms()
 
     for i in 1:length(types)
         type = types[i]
-        power_law = []
+        gamma2 = []
         for j in 2:maximumPrimeBlockSize
             primeBlockSize = j
             for k in 1:factorial(primeBlockSize)
-                fit = readdlm("DATA/POWER_SPECTRA_STATIONARY_FIT/fit_powerspectra_n_0_$(k)_$(type)_mVectorSize_$(mVectorSize)_MaxRand_$(MaxRand)_primeBlockSize_$(primeBlockSize).csv")
-                power_law = vcat(power_law,fit[2])
+                γ₂ = readdlm("DATA/GAMMA_2/gamma_2_n_0_$(k)_$(type)_mVectorSize_$(mVectorSize)_MaxRand_$(MaxRand)_primeBlockSize_$(primeBlockSize).csv", header = false)
+                gamma2 = vcat(gamma2,γ₂[1])
             end
         end
-        average = mean(power_law)
-        stdDeviation = std(power_law)
-        topPlots = histogram!(power_law,
+        average = mean(gamma2)
+        stdDeviation = std(gamma2)
+        topPlots = histogram!(gamma2,
             subplot = i,
             bins = 30,
             title = L"\mathrm{%$(type)} \ \mathrm{initial} \ m-\mathrm{vector}",
-            c=:red,
+            c=:orange,
             fontfamily = (12,"Computer Modern"),
-            xlabel = L"\beta",
+            xlabel = L"\gamma_2",
             yguidefontrotation = -90,
             framestyle = :box,
             legendposition = :topright,
             label = :false
             )
-        topPlots = plot!([mean(power_law);mean(power_law)],
-            [0;150],
+        topPlots = plot!([mean(gamma2);mean(gamma2)],
+            [0;250],
             subplot = i,
             ls = :dash,
             lw = 1.5,
             lc = :black,
-            label = L"\langle\beta\rangle =  %$(Float16(average)) \pm %$(Float16(stdDeviation[1]))"
+            label = L"\langle\gamma_2\rangle =  %$(Float16(average)) \pm %$(Float16(stdDeviation[1]))"
             )
     end
-    topPlots = plot!(subplot = 1, ylabel = L"D(\beta)", annotation = ((0.1,0.9), L"(a)"))
+    topPlots = plot!(subplot = 1, ylabel = L"D(\gamma_2)", annotation = ((0.1,0.9), L"(a)"))
     topPlots = plot!(subplot = 2, yticks = false, annotation = ((0.1,0.9), L"(b)"))
 
     bottomPlot = plot(size = (300,150))
     types = ["Random", "Prime"]
-    power_law = []
+    gamma2 = []
 
     for i in 1:length(types)
             type = types[i]
             for j in 2:maximumPrimeBlockSize
                 primeBlockSize = j
                 for k in 1:factorial(primeBlockSize)
-                    fit = readdlm("DATA/POWER_SPECTRA_STATIONARY_FIT/fit_powerspectra_n_0_$(k)_$(type)_mVectorSize_$(mVectorSize)_MaxRand_$(MaxRand)_primeBlockSize_$(primeBlockSize).csv")
-                    power_law = vcat(power_law,fit[2])
+                    γ₂ = readdlm("DATA/GAMMA_2/gamma_2_n_0_$(k)_$(type)_mVectorSize_$(mVectorSize)_MaxRand_$(MaxRand)_primeBlockSize_$(primeBlockSize).csv", header = false)
+                    gamma2 = vcat(gamma2,γ₂[1])
                 end
             end
     end
-    average = mean(power_law)
-    stdDeviation = std(power_law)
-    bottomPlot = histogram!(power_law,
-        c=:red,
+    average = mean(gamma2)
+    stdDeviation = std(gamma2)
+    bottomPlot = histogram!(gamma2,
+        c=:orange,
         fontfamily = "Computer Modern",
-        xlabel = L"\beta",
-        ylabel = L"D(\beta)",
+        xlabel = L"\gamma_2",
+        ylabel = L"D(\gamma_2)",
         yguidefontrotation = -90,
         left_margin = 3mm,
         framestyle = :box,
@@ -80,11 +80,11 @@ function histograms()
         #top_margin = mm,
         label = :false
         )
-    bottomPlot = plot!([mean(power_law);mean(power_law)],[0;200],
+    bottomPlot = plot!([mean(gamma2);mean(gamma2)],[0;310],
         ls = :dash,
         lw = 1.5,
         lc = :black,
-        label = label = L"\langle\beta\rangle=  %$(Float16(average)) \pm %$(Float16(stdDeviation[1]))"
+        label = label = L"\langle\gamma_2\rangle=  %$(Float16(average)) \pm %$(Float16(stdDeviation[1]))"
         )
     bottomPlot = plot!(annotation = ((0.05,0.9), L"(c)"))
 
@@ -94,7 +94,7 @@ function histograms()
         dpi = 500,
         left_margin = 6mm
         )
-    png(plotfinal, string(pasta,"/power_spectra_histogram"))
+    png(plotfinal, string(pasta,"/gamma_2_histogram"))
 end
 
 histograms()
